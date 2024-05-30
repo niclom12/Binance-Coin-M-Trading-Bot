fdd02f33fede2d70e4103c864edbe9adb5d8ffc2
niclom12 <142734478+niclom12@users.noreply.github.com>
2024-05-30 13:42:34 +0200

    Using varying leverage

--- Main.py
+++ Main.py
@@ -8,27 +8,26 @@ import pandas as pd
 from binance.exceptions import BinanceAPIException
 
 
-tokens = ["BTCUSD_PERP", "DOGEUSD_PERP","SOLUSD_PERP", "LINKUSD_PERP", "BNBUSD_PERP"]
+
+tokens = ["BTCUSD_PERP", "DOGEUSD_PERP","SOLUSD_PERP", "LINKUSD_PERP", "BNBUSD_PERP", "ETHUSD_PERP"]
 def algo(data):
     return logic(data)
 
 
 def run_algo():
-    print("STARTING RUN.......................")
+    time.sleep(3)
     client_info = pd.read_excel('Users.xlsx')
     first_row = client_info.iloc[0]
     binance = Binance(str(first_row['api']), str(first_row['private']))
     for token in tokens:
-        print(token)
         try:
             data = binance.get_klines(token, 150)
-            side = algo(data)
+            side, hull = algo(data)
             if (side == "Buy"):
                 for index, row in client_info.iterrows():
                     try:
                         client = Binance(row.api, row.private)
-                        #quant = int(client.get_buy_amount(token))
-                        quant = 1
+                        quant = int(client.get_buy_amount(token, hull))
                         client.make_trade_market("Buy", token, quant)
                         positions = row.positions.split(",")
                         for i, position in enumerate(positions):
@@ -57,14 +56,27 @@ def run_algo():
         except BinanceAPIException as e:
             log_error(e)
             
-    client_info.to_excel('Users.xlsx', sheet_name='Sheet1', index=False)
+    client_info.to_excel('Users.xlsx', index=False)
 
-    
+
+
+log_start()
+schedule.every().day.at("02:00").do(run_algo)
+schedule.every().day.at("06:00").do(run_algo)
+schedule.every().day.at("10:00").do(run_algo)
+schedule.every().day.at("14:00").do(run_algo)
+schedule.every().day.at("18:00").do(run_algo)
+schedule.every().day.at("22:00").do(run_algo)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
         
-        
+
 """
 log_start()
-now = datetime.now()
+now = datetime.datetime.now()
 seconds_until_half_hour = (30 - (now.minute % 30)) * 60 - now.second
 time.sleep(seconds_until_half_hour)
 run_algo()
@@ -72,9 +84,10 @@ schedule.every(30).minutes.do(run_algo)
 while True:
     schedule.run_pending()
     time.sleep(1)
-"""    
+"""  
 
-
+  
+"""
 def run_job_at_next_minute():
     current_time = datetime.datetime.now()
     delay = 60 - current_time.second + 2
@@ -85,15 +98,6 @@ log_start()
 while True:
     run_job_at_next_minute()
 
-    
-"""
-schedule.every().day.at("00:01").do(run_algo)
-schedule.every().day.at("04:01").do(run_algo)
-schedule.every().day.at("08:01").do(run_algo)
-schedule.every().day.at("12:01").do(run_algo)
-schedule.every().day.at("20:01").do(run_algo)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
 """
